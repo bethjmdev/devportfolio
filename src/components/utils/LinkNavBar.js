e384779fbebd1afa3a69f7faebbbcf784307d1ca
bethjm <b.jeanne.mills@gmail.com>
2024-11-20 09:04:24 -0800

    made linknavbar clickable links

--- src/components/utils/LinkNavBar.js
+++ src/components/utils/LinkNavBar.js
@@ -6,17 +6,48 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
 
 function LinkNavBar({ className }) {
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText("bethjmdev@gmail.com")
+      .then(() => {
+        alert("Email copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy email: ", err);
+      });
+  };
+
   return (
     <div className={className}>
       <div className="linkbar-container">
         {/* LinkedIn */}
-        <FontAwesomeIcon icon={faLinkedin} />
+        <a
+          href="https://www.linkedin.com/in/bethjmdev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
         {/* GitHub */}
-        <FontAwesomeIcon icon={faGithub} />
-        {/* Resume ADD: on hover "resume"*/}
-        <FontAwesomeIcon icon={faFile} />
-        {/* Email?? */}
-        <FontAwesomeIcon icon={faEnvelope} />
+        <a
+          href="https://github.com/bethjmdev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        {/* resume */}
+        <a
+          href="https://docs.google.com/document/d/1SJ3WMdccEwwg8AqQ0RmXuUAr5-CEZ7up8gNjkXu7N5Y/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFile} />
+        </a>
+        {/* Email */}
+        <a onClick={copyToClipboard} style={{ cursor: "pointer" }}>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
       </div>
     </div>
   );
