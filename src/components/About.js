90fe8d731a04cdfec4db36bf783b708d34e1a2bd
bethjm <b.jeanne.mills@gmail.com>
2025-08-20 16:07:17 -0400

    updated about and experience

--- src/components/About.js
+++ src/components/About.js
@@ -37,6 +37,7 @@ function About() {
     "Postman",
     "Agile",
     "Unit Testing",
+    "Electron"
   ];
 
   return (
@@ -53,39 +54,28 @@ function About() {
           <div className="know-me">
             <h4>Get to know me!</h4>
             <p>
-              I’m a full-stack developer at Hōmbaez, a fintech and proptech
-              company, where I build their B2B web app and B2C mobile app. My
-              work focuses on creating tools that help home buyers represent
-              themselves and save money in the process.
+              I’m a Core Developer at YOR.AI. My work focuses on creating 
+              user interfaces for products that replace manual workflows with private AI agents.
             </p>
             <br />
             <p>
               I’m the kind of person who dives into big challenges, even when I
-              don’t have all the skills at first. I trust myself to figure it
-              out, learn what’s needed, and make it happen along the way. I
-              thrive in environments where the team is eager to learn and grow
-              together—I truly believe that a rising tide lifts all ships.{" "}
+              don’t have all the skills at first. I trust myself to learn what’s needed and
+              find the right people who can help me to get the job done. 
             </p>
             <br />
 
+            <p>I thrive in environments where the team is eager to learn and grow
+              together, communication is clear, and feedback is frequent.</p>
+              <br />
             <p>
-              I’m a big believer in clear and open communication—if anything,
-              I’d rather over-communicate than leave things unclear. I also
-              really value kind, actionable feedback, as it helps me grow and
-              become the engineer I want to be.
+              I recently returned home after a year long road trip across the US. We lived in California, Arizona, and Utah, and Idaho. Ask me about my favorite parts of the trip!
             </p>
             <br />
-
-            <p>
-              Right now, I’m looking for a remote position while I explore the
-              United States with my cats, boyfriend, and everything we can fit
-              in the back of our RAV4. We’ve spent the last three months in
-              Northern California and are off to Arizona next. Feel free to ask
-              me about our other stops! When I’m not coding, you can find me
-              obsessing over specialty coffee, whipping up comfort meals that’ll
-              put you into a food coma, or hiking to the top of mountain peaks.
-            </p>
-          </div>
+            <p>I am based out of Manchester, NH and am currently looking for a remote position. I am open to a hyrid position for the right company. I would love to do full stack or front end engineering for a start up or a small company.</p>
+         <br/>
+         <p>When I’m not coding, you can find me obsessing over specialty coffee, whipping up comfort meals that’ll put you into a food coma, or hiking to the top of mountain peaks.</p>
+         </div>
           <div className="skills">
             <h3>Skills</h3>
             <div className="skill-icons">
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,6 +5,7 @@ import ProjectContainer from "../components/utils/projects/ProjectContainer";
 import Homhub from "../images/HomHub.png";
 import HomApp from "../images/HApp.png";
 import Waggy from "../images/WaggyApp.png";
+import YorAI from "../images/yor_ai_logo.png";
 
 function Experience() {
   return (
@@ -16,30 +17,45 @@ function Experience() {
           <hr id="hr" />
         </div>
         <ProjectContainer
-          website={"https://www.hombaez.com/"}
+          website={"https://www.theyor.com/"}
+          image={YorAI}
+          linkedIn={
+            "https://www.linkedin.com/company/yor-ai/about/"
+          }
+          direction="row-reverse"
+          HeaderText={"AI Platforms at YOR.AI"}
+          Description={
+          "YOR.AI creates custom and private AI agents and user interfaces for companies to streamline operations. Our AI agents can be local, cloud based, or hybrid. We have a strong focus on compliance and privacy. As the Core Developer, I build user interfaces for various companies utilizing our platforms and make the UI tailored to the individual needs of the organization. I work with the founders of my company to communicate with the clients and understand their needs so we can realisticallybring their expecations to life."          }
+          dates={"Full Stack Developer | March 2024 - Current"}
+          TechText={
+            "React.js, Vite, RESTful Routes, Electron, reusable components, JWT Tokens, Node.js, CSS3"
+          }
+        />
+        <ProjectContainer
+          website={"https://www.theyor.com/"}
           image={Homhub}
           linkedIn={
             "https://www.linkedin.com/company/hombaez/posts/?feedView=all"
           }
           direction="row-reverse"
-          HeaderText={"HomHub at Hombaez"}
+          HeaderText={"HomHub at YOR.AI (previosuly Hombaez)"}
           Description={
             "Hōmbaez is a real estate platform that enables lenders to purchase exclusive zip codes for advertising, connecting them with homebuyers. I implemented features like real-time messaging, deal tracking, secure document upload, zip code purchasing for advertisement, and profile management. The platform is optimized for performance, with 64.3MB memory usage and a 798 ms API response time."
           }
-          dates={"Full Stack Developer | July 2024 - Present"}
+          dates={"Full Stack Developer | March 2024 - Jan 2025"}
           TechText={
             "React.js, Firebase, AWS, Twilio, SendGrid, Google Cloud Functions, Google Maps API, CSS, RESTful Routes"
           }
         />
         <ProjectContainer
-          website={"https://www.hombaez.com/"}
+          website={"https://www.theyor.com/"}
           image={HomApp}
           direction="row-reverse"
           linkedIn={
             "https://www.linkedin.com/company/hombaez/posts/?feedView=all"
           }
-          HeaderText={"Hombaez Mobile App"}
-          dates={"Mobile Developer | July 2024 - Present"}
+          HeaderText={"Hombaez Mobile App at YOR.AI (previosuly Hombaez)"}
+          dates={"Mobile Developer | July 2024 - Dec 2024"}
           Description={
             "I built key features for Hōmbaez, a real estate platform connecting buyers and lenders. This includes Hōmbaez's loan pre-approval document upload with an intuitive interface, achieving 3 MB/s upload speeds, and a real-time messaging feature using scalable Firebase architecture, supporting 200 users with under 100ms latency."
           }
